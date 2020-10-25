14128cc3afeff2950daca9348152faf37ba74ab3
xhasu <ing.xhasu@gmail.com>
2020-10-25 13:20:48 -0500

    Initial Project

---
+++ js/app.js
@@ -0,0 +1,75 @@
+(function(){
+
+  // fancybox
+  $.fancybox.defaults.backFocus = false;
+
+  $('.fancybox-galley_launch .swiper-slide:not(.swiper-slide-duplicate) [data-fancybox="galley_launch"]').fancybox({});
+
+
+  // swiper
+  let swipers = document.querySelectorAll('.showcase-swiper');
+
+  swipers.forEach(swiper => {
+
+    let el = swiper.querySelector('.swiper-container');
+    let paginationEl = swiper.querySelector('.swiper-pagination');
+  
+    new Swiper(el, {
+      loop: true,
+      threshold: 20,
+      pagination: {
+        el: paginationEl,
+        type:  'bullets',
+        clickable: true,
+      }
+    });
+
+  });
+
+  // scrollview
+  let toView = document.querySelectorAll('[data-to-view^="#"]');
+  let toViewSize = toView.length;
+
+  for( let i  = 0; i < toViewSize; i++){
+    toView.item(i).addEventListener('click', function(e) {
+      e.preventDefault()
+      document.querySelector(this.getAttribute('data-to-view')).scrollIntoView({behavior: 'smooth'});
+    })
+  }
+
+  // header sticky
+  let throttleTimeout = null;
+  let stickyObserver = document.querySelector('.sticky-observer');
+  let headerElement = document.querySelector('.header');
+
+  function handleSticky () {
+
+    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    let diffTop = stickyObserver.getBoundingClientRect().y + scrolled;
+
+    if (scrolled >= diffTop - 25) {
+      headerElement.classList.add('isSticky');
+    } else {
+      headerElement.classList.remove('isSticky');
+    }
+
+    throttleTimeout = null;
+  };
+
+  function _throttleSticky(){
+    if (throttleTimeout == null)
+      throttleTimeout = setTimeout(handleSticky, 120);
+  }
+
+  if( stickyObserver ){
+    window.addEventListener("scroll", _throttleSticky);
+  }
+
+  // load background
+  /*let bgPage = document.querySelector('[data-background]');
+  if( bgPage ) {
+    let bgPageSrc = bgPage.querySelector('img').src;
+    bgPage.style.backgroundImage = "url('" + bgPageSrc + "')";
+  }*/
+
+})();
